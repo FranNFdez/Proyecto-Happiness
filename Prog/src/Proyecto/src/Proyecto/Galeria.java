533dcec894f6eb790c3a17ed56c5d5bf907d8bb1
Francisco Javier Nuñez Fernandez <elfrancis168@gmail.com>
2026-04-08 12:33:26 +0200

    java crear y eliminar eventos y configuracion git bash por errores frecuentes

--- Prog/src/Proyecto/src/Proyecto/Galeria.java
+++ Prog/src/Proyecto/src/Proyecto/Galeria.java
@@ -2,4 +2,22 @@ package Proyecto;
 
 public class Galeria {
 
+    private int id;
+    private String titulo;
+    private int idEvento;
+
+    public Galeria(int id, String titulo, int idEvento) {
+        this.id = id;
+        this.titulo = titulo;
+        this.idEvento = idEvento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + titulo;
+    }
 }
