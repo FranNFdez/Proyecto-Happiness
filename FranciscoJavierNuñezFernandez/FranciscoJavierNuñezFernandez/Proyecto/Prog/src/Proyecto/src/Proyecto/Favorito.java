39866e50db2d9f63f10f8f0a95f1329ed8ebe650
Francisco Javier Nuñez Fernandez <elfrancis168@gmail.com>
2026-04-13 13:09:03 +0200

    Deje casi terminados los eventos pasados con las galerias y estoy siguiendo editando las demas imagenes para mañana intentar terminar los pasados el historial y algo mas

--- FranciscoJavierNuñezFernandez/FranciscoJavierNuñezFernandez/Proyecto/Prog/src/Proyecto/src/Proyecto/Favorito.java
+++ FranciscoJavierNuñezFernandez/FranciscoJavierNuñezFernandez/Proyecto/Prog/src/Proyecto/src/Proyecto/Favorito.java
@@ -2,4 +2,32 @@ package Proyecto;
 
 public class Favorito {
 
+    private String correoUsuario;
+    private int idEvento;
+
+    public Favorito(String correoUsuario, int idEvento) {
+        this.correoUsuario = correoUsuario;
+        this.idEvento = idEvento;
+    }
+
+    public String getCorreoUsuario() {
+        return correoUsuario;
+    }
+
+    public int getIdEvento() {
+        return idEvento;
+    }
+
+    public void setCorreoUsuario(String correoUsuario) {
+        this.correoUsuario = correoUsuario;
+    }
+
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    @Override
+    public String toString() {
+        return correoUsuario + " - Evento ID: " + idEvento;
+    }
 }
